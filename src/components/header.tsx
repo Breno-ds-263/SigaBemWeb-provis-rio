0c80ab9bcb4a97c22d0195e0b7208beb028cf7b2
Breno santos <Breno.ds263@gmail.com>
2025-10-19 00:41:20 -0300

    Terminado visual header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,15 +5,15 @@
 export default function Header(){
 
   return(
-    <div className = "flex flex-row bg-neutral-900">
-    <a>Sigabem</a>
-        <nav>
-            <a href="/sigabem/home">Home</a>
-            <a href="/sigabem/linhas">Linhas</a>
-            <a href="/sigabem/notifications">Notificações</a>
-            <a href="/sigabem/administrators">Administração</a>
-        </nav>
-    </div>
+    <div className="w-full px-5 py-4 flex justify-between items-center bg-green-500 border border-green-700 shadow-md">
+    <a href="#" className="text-[24px] text-white font-bold no-underline">SigaBem</a>
+        <nav className="flex items-center">
+      <a href="#" className="relative text-[16px] text-white font-medium no-underline">Home</a>
+      <a href="#" className="relative text-[16px] text-white font-medium no-underline ml-5">Sobre</a>
+      <a href="#" className="relative text-[16px] text-white font-medium no-underline ml-5">Serviços</a>
+      <a href="#" className="relative text-[16px] text-white font-medium no-underline ml-5">Contato</a>
+    </nav>
+        </div>
   )
 
 }
