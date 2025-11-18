65413b8ea44f41d9c18543214141b2cad901c387
AlissonCoding <alisson_r2r4@hotmail.com>
2025-11-17 23:38:30 -0300

    feature-fix: button

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,6 +1,4 @@
-import Header from "../components/Header";
 import NotificationTable from "../components/NotificationTable";
-import TabbleButton from "../components/Button";
 
 export default function Notification(){
     return(
