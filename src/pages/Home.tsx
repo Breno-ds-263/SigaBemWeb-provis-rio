4a673d04c59eb41dd1d91bb4a758eae8c9dfee0a
Breno-ds-263 <Breno.ds263@gmail.com>
2025-11-19 18:01:01 -0300

    Continuação FilterContainer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,26 +5,14 @@ import Map from "../components/map";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  const [peopleFilters, setPeopleFilters] = useState({
-    minAge: 0,
-    maxAge: 100,
-    gender: "all",
-    disability: "all",
-    city: "Recife",
-    neigh: "",
-  });
-
-  const [stopFilters, setStopFilters] = useState({ city: "Recife", neigh: "" });
-
+ 
   return (
     <div className="flex flex-col h-screen w-full">
       <Header />
       <main className="flex flex-1 flex-col md:flex-row overflow-hidden">
         <aside className="w-full md:w-[380px] bg-white shadow-md p-4 overflow-y-auto">
-          <FiltersContainer
-            onSearchPeople={setPeopleFilters}
-            onSearchStops={setStopFilters}
-          />
+          <FiltersContainer/>
+          
         </aside>
 
         <section className="flex-1 h-[400px] md:h-auto">
